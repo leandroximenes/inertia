be5a32413e6eec27486eba64c9cc11e2a22896b0
leandro <leandroj.r.ximenes@gmail.com>
2023-03-15 14:01:32 +0000

    merged with L16-end

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -37,6 +37,7 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request): array
     {
         return array_merge(parent::share($request), [
+            'appName' => config('app.name'),
             'user' => [
                 'name' => 'Leandro Ximenes',
                 'email' => 'leandroj.r.ximenes@gmail.com'
--- resources/js/Layout/MainLayout.vue
+++ resources/js/Layout/MainLayout.vue
@@ -8,18 +8,18 @@
     <main class="min-h-screen flex flex-col h-screen">
         <header class="p-5 text-center bg-gray-100"> 
             <span class="float-left">Hello, {{ user.name }}</span>
-            <span class="text-lg font-bold">My App</span>
+            <span class="text-lg font-bold">{{ $page.props.appName}}</span>
         </header>
         <div class="flex-1 flex flex-row">
             <nav class="order-first sm:w-32  overflow-y-auto bg-gray-200">
                 <ul class="mt-2 space-y-5 pt-5 ">
-                    <li><Link class="menu-link" href="/">Home</Link></li>
-                    <li><Link class="menu-link" href="/users">Users</Link></li>
-                    <li><Link class="menu-link" href="/settings">Settings</Link></li>
+                    <li><Link class="menu-link" href="/" :class="{ 'active': $page.component.startsWith('Home')}">Home</Link></li>
+                    <li><Link class="menu-link" href="/users" :class="{ 'active': $page.component.startsWith('Users')}">Users</Link></li>
+                    <li><Link class="menu-link" href="/settings" :class="{ 'active': $page.component.startsWith('Settings')}">Settings</Link></li>
                 </ul>
             </nav>
             <main class="flex-1  overflow-y-auto p-2"><slot></slot></main>
         </div>
-        <footer class="text-center">Footer</footer>
+        <footer class="text-center bg-gray-800 text-white">© All rights reserved</footer>
     </main>
 </template>
