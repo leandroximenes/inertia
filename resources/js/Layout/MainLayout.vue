711e318419fc309a96e1faac6b242e97c803b7ff
leandro <leandroj.r.ximenes@gmail.com>
2023-08-09 15:50:50 +0000

    menu done

--- resources/js/Layout/MainLayout.vue
+++ resources/js/Layout/MainLayout.vue
@@ -1,25 +1,50 @@
 <script setup>
+import { ref } from 'vue'
 import ComponentModal from '../Components/Modal.vue'
 import { usePage } from '@inertiajs/vue3'
 import { computed } from 'vue'
-import { HomeIcon, UsersIcon, CogIcon } from '@heroicons/vue/24/solid'
+import {
+  HomeIcon,
+  UsersIcon,
+  CogIcon,
+  Bars3Icon,
+} from '@heroicons/vue/24/solid'
 const user = computed(() => usePage().props.user)
+
+const isMobile = ref(window.innerWidth < 640)
+const menu = ref({
+  show: !isMobile.value,
+})
+const toggleMenu = () => {
+  if (isMobile.value) {
+    menu.value.show = !menu.value.show
+  }
+}
 </script>
 
 <template>
   <main class="min-h-screen flex flex-col h-screen">
-    <header class="p-5 bg-gray-200">
-      <span class="text-lg font-bold">{{ $page.props.appName }}</span>
-      <Link
-        href="/logout"
-        method="post"
-        class="float-right mx-2 font-bold hover:underline"
-        >Log Out</Link
-      >
-      <span class="float-right">Hello, {{ user.name }}</span>
+    <header class="p-5 bg-gray-200 flex justify-between">
+      <span class="text-lg font-bold flex">
+        <Bars3Icon
+          class="w-6 h-6 mr-1 my-auto cursor-pointer"
+          @click="toggleMenu"
+          v-show="isMobile"
+        />
+        {{ $page.props.appName }}
+      </span>
+      <span>
+        <Link
+          href="/logout"
+          method="post"
+          class="float-right mx-2 font-bold hover:underline"
+          >Sair</Link
+        >
+        <span class="float-right">Hello, {{ user.name }}</span>
+      </span>
     </header>
     <div class="flex-1 flex flex-row">
-      <nav class="order-first sm:w-40 overflow-y-auto bg-gray-300">
+      <nav class="menu" v-show="menu.show">
         <ul class="pt-1">
           <li>
             <Link
@@ -28,6 +53,7 @@ const user = computed(() => usePage().props.user)
               :class="{
                 'menu-link-active': $page.component.startsWith('Home'),
               }"
+              @click="toggleMenu"
             >
               <HomeIcon class="w-4 h-4" />
               <span>Início</span>
@@ -40,6 +66,7 @@ const user = computed(() => usePage().props.user)
               :class="{
                 'menu-link-active': $page.component.startsWith('Users'),
               }"
+              @click="toggleMenu"
             >
               <UsersIcon class="w-4 h-4" />
               <span>Usuários</span>
@@ -52,6 +79,7 @@ const user = computed(() => usePage().props.user)
               :class="{
                 'menu-link-active': $page.component.startsWith('Settings'),
               }"
+              @click="toggleMenu"
             >
               <CogIcon class="w-4 h-4" />
               <span>Configurações</span>
